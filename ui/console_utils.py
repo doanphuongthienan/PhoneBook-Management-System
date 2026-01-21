94a8091545b5fcc41f63d392ec18b856a86c1930
Doan Phuong Thien An <andpt2350@ut.edu.vn>
2026-01-21 20:51:54 +0700

    Update console utilities

--- ui/console_utils.py
+++ ui/console_utils.py
@@ -1,5 +1,17 @@
-def show_message(msg):
-    print(msg)
+def clear_screen():
+    print("\n" * 50)
 
-def get_input(prompt):
-    return input(prompt)
+
+def pause():
+    input("Nhấn Enter để tiếp tục...")
+
+
+def print_header(title):
+    clear_screen()
+    print("=" * 40)
+    print(title.upper())
+    print("=" * 40)
+
+
+def show_message(message):
+    print(message)
